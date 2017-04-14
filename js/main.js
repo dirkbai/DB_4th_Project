97325b5a766576b6ae67177dab15d6da8dc74e45
dirkbai <baigent.dirk@gmail.com>
2017-04-14 19:56:07 +0200

    search

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ $('.Photos_wrapper').magnificPopup({
 		type: 'image',
 		closeOnContentClick: false,
 		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		mainClass: 'mfp-img-mobile',
 		image: {
 			verticalFit: true,
 			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+				return item.el.attr('title');
 			}
 		},
 		gallery: {
@@ -22,3 +22,24 @@ $('.Photos_wrapper').magnificPopup({
 		}
 
 	});
+
+	$(document).ready(function(){
+    $("#Search").keyup(function(){
+			var filter = $(this).val();
+			    $(".Photos_wrapper a").each(function(){
+				// If the list item does not contain the text phrase fade it out
+            if ($(this).attr('title').search(filter) < 0) {
+                $(this).hide(600);
+
+            // Show the list item if the phrase matches and increase the count by 1
+            } else {
+                $(this).show(600);
+                //count++;
+            }
+        });
+
+        // Update the count
+        //var numberItems = count;
+        //$("#filter-count").text("Number of Comments = "+count);
+    });
+});
